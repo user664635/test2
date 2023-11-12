1f039a8f91edf6892460d8f1c388a43bcdfc71ef
Your Name <you@example.com>
2023-11-12 20:24:02 +0800

    key

--- driver/key.h
+++ driver/key.h
@@ -8,12 +8,12 @@ static volatile u8 KEY;
 #endif
 
 inline u16 key16(void) {
-  u8 key[2];
-  KEY = 0b11111110, key[0] = KEY >> 4;
-  KEY = 0b11111101, key[0] |= KEY & 0xf0;
-  KEY = 0b11111011, key[1] = KEY >> 4;
-  KEY = 0b11110111, key[1] |= KEY & 0xf0;
-  return *(u16 *)key;
+  Hex4 key;
+  KEY = 0b11101111, key.hexs.h0 = KEY;
+  KEY = 0b11011111, key.hexs.h1 = KEY;
+  KEY = 0b10111111, key.hexs.h2 = KEY;
+  KEY = 0b01111111, key.hexs.h3 = KEY;
+  return key.word;
 }
 
 #endif
