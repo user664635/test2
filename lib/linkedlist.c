e0c98a8372178afb6e2fd8b26a11eae63c81e024
Your Name <you@example.com>
2023-11-18 14:57:26 +0800

    sllist insert

---
+++ lib/linkedlist.c
@@ -0,0 +1,23 @@
+#ifndef LINKEDLIST_C
+#define LINKEDLIST_C
+
+#include <stdlib.h>
+#ifndef ElementType
+#define ElementType int
+#endif
+
+typedef struct SingleLinkedList SingleLinkedList;
+
+struct SingleLinkedList {
+  ElementType element;
+  SingleLinkedList *next;
+};
+
+void sllist_insert(SingleLinkedList *node, ElementType value) {
+  SingleLinkedList *newnode = malloc(sizeof(SingleLinkedList));
+  newnode->element = value;
+  newnode->next = node->next;
+  node->next = newnode;
+}
+
+#endif
