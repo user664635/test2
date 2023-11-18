e127ee428ee3fdae97a67c690c87bb966fa233d5
Your Name <you@example.com>
2023-11-18 22:05:09 +0800

    contact1

--- contact/main.c
+++ contact/main.c
@@ -1,5 +1,3 @@
-
-
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -10,22 +8,44 @@ typedef struct {
   char name[16];
 } Contact;
 
-typedef struct{
+typedef struct {
   Contact *data;
-  size_t len,cap;
+  size_t len, cap;
 } Contacts;
 
-void printcontacts(Contact *x, size_t n) {
-  for (int i = 0; i < n; ++i)
-    printf("%s:%lu\n", x[i].name, x[i].num);
+#define INIT_CAP 128
+Contacts contacts_init(void) {
+  Contacts contacts;
+  contacts.cap = INIT_CAP;
+  contacts.len = 0;
+  contacts.data = calloc(INIT_CAP, sizeof(Contact));
+  return contacts;
 }
 
-int main(void){
-      Contact *contacts = calloc(n, sizeof(Contact));
-  printf("%lu\n", fread(contacts, sizeof(Contact), n, fopen("data", "r")));
-  printcontacts(contacts, n);
-  strcpy(contacts[0].name, "bbb");
-  contacts[0].num = 163467;
+void contacts_read(Contacts contacts) {
+  size_t len = fread(contacts.data, sizeof(Contact), -1, fopen("data", "r"));
+  contacts.len = len;
+}
 
-  printf("%lu\n", fwrite(contacts, sizeof(Contact), n, fopen("data", "w")));
+void contacts_write(Contacts contacts) {
+  fwrite(contacts.data, sizeof(Contact), contacts.len, fopen("data", "w"));
+}
+
+void contacts_add(Contacts contacts,char *name,uint64_t num){
+    Contact contact = {.num = num};
+    strcpy(contact.name, name);
+    contacts.data[contacts.len++] = contact;
+}
+
+void printcontacts(Contacts contacts) {
+  for (int i = 0; i < contacts.len; ++i)
+    printf("%s:%lu\n", contacts.data[i].name, contacts.data[i].num);
+}
+
+int main(void) {
+  Contacts contacts = contacts_init();
+  contacts_read(contacts);
+  printcontacts(contacts);
+  contacts_add(contacts, "asdf", 12354346);
+  contacts_write(contacts);
 }
