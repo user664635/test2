d9b8b7b98cf8ea00f4b107184a05252eb8665ec7
Your Name <you@example.com>
2023-11-22 18:39:13 +0800

    def

--- def.h
+++ def.h
@@ -1,16 +1,18 @@
 #ifndef DEF_H
 #define DEF_H
 
-#include <limits.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <tgmath.h>
+
+#define PHI 1.61803398874989484820458683436563812
+#define PHI1 -0.61803398874989484820458683436563812
+#define PI 3.14159265358979323846264338327950288
 
 typedef uint8_t u8;
 typedef uint16_t u16;
 typedef uint32_t u32;
 typedef uint64_t u64;
-typedef unsigned __int128 u128;
 typedef size_t usize;
 
 typedef uint_fast8_t fu8;
@@ -27,7 +29,6 @@ typedef int8_t i8;
 typedef int16_t i16;
 typedef int32_t i32;
 typedef int64_t i64;
-typedef __int128 i128;
 typedef ssize_t isize;
 
 typedef int_fast8_t fi8;
@@ -40,6 +41,9 @@ typedef int_least16_t li16;
 typedef int_least32_t li32;
 typedef int_least64_t li64;
 
+#ifndef SDCC
+typedef unsigned __int128 u128;
+typedef __int128 i128;
 typedef _Float16 f16;
 typedef _Float32 f32;
 typedef _Float64 f64;
@@ -49,7 +53,7 @@ typedef __float128 f128;
 typedef _Float64 f64x2 __attribute__((vector_size(16)));
 
 #define print(str) fputs(str, stdout)
-
+#define putdigit(x) putchar(x + '0')
 #define printuint(digit)                                                       \
   if (x < 10) {                                                                \
     putchar('0' + x);                                                          \
@@ -58,30 +62,50 @@ typedef _Float64 f64x2 __attribute__((vector_size(16)));
   char str[digit];                                                             \
   uint_fast8_t i = 0;                                                          \
   for (; x; x /= 10)                                                           \
-    str[i++] = x % 10 + '0';                                                   \
+    str[i++] = x % 10;                                                         \
   while (i)                                                                    \
-  putchar(str[--i])
+  putdigit(str[--i])
 
-#define printint(digit)                                                        \
+#define printsign()                                                            \
   if (x < 0) {                                                                 \
     x = -x;                                                                    \
     putchar('-');                                                              \
-  }                                                                            \
+  }
+
+#define printint(digit)                                                        \
+  printsign();                                                                 \
   printuint(digit);
 
 static inline void printu128(u128 x) { printuint(39); }
 static inline void printi128(i128 x) { printint(39); }
-static inline void printu64(u64 x) { printuint(20); }
-static inline void printi64(i64 x) { printint(20); }
-static inline void printu32(u32 x) { printuint(10); }
-static inline void printi32(i32 x) { printint(10); }
-static inline void printu16(u16 x) { printuint(5); }
-static inline void printi16(i16 x) { printint(5); }
-static inline void printu8(u8 x) { printuint(3); }
-static inline void printi8(i8 x) { printint(3); }
+static inline void printu64(fu64 x) { printuint(20); }
+static inline void printi64(fi64 x) { printint(20); }
+static inline void printu32(fu32 x) { printuint(10); }
+static inline void printi32(fi32 x) { printint(10); }
+static inline void printu16(fu16 x) { printuint(5); }
+static inline void printi16(fi16 x) { printint(5); }
+static inline void printu8(fu8 x) { printuint(3); }
+static inline void printi8(fi8 x) { printint(3); }
 
-// #undef printuint
-// #undef printint
+#define F16MAX 65504
+static inline void printf16(f16 x) {
+  printsign();
+  if (x <= F16MAX) {
+    fi16 trunc = x;
+    printu16(trunc);
+    putchar('.');
+    while (x) {
+      x -= trunc;
+      x *= 10;
+      trunc = x;
+      putdigit(trunc);
+    }
+  } else if (x > F16MAX)
+    print("inf");
+  else
+    print("nan");
+  putchar('\n');
+}
 
 static inline void printf32(float x) {
 
@@ -97,4 +121,5 @@ static inline void printf32(float x) {
     putchar(str[--i]);
 }
 
+#endif
 #endif
