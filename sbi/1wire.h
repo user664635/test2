f6ffd105e6ad05c80efeff3b582e919e9d43544c
Your Name <you@example.com>
2023-11-23 15:50:38 +0800

    1wire

--- sbi/1wire.h
+++ sbi/1wire.h
@@ -1,52 +1,54 @@
 #ifndef WIRE1_H
 #define WIRE1_H
 
-#define delay(t) for (register uint8_t i = t; --i;)
+#include "../lib/def.h"
+#define delay(t) for (u8 i = t; --i;)
 
-#define wire1_init(DQ)                                                         \
-  do {                                                                         \
-    DQ = 1, DQ = 0;                                                            \
-    delay(240);                                                                \
-    DQ = 1;                                                                    \
-    delay(240);                                                                \
-  } while (0)
+#ifndef DQ
+volatile static u8 DQ;
+#endif
 
-#define wire1_send_bit(bit, DQ)                                                \
-  do {                                                                         \
-    DQ = 1, DQ = 0, DQ = bit;                                                  \
-    delay(40);                                                                 \
-  } while (0)
+static inline void wire1_init(void) {
+  DQ = 1, DQ = 0;
+  delay(240);
+  DQ = 1;
+  delay(240);
+}
 
-#define wire1_recv_bit(out, p, DQ)                                             \
-  do {                                                                         \
-    DQ = 1, DQ = 0, DQ = 1;                                                    \
-    delay(10);                                                                 \
-    out |= DQ << p;                                                            \
-    delay(30);                                                                 \
-  } while (0)
+static inline void wire1_send_bit(u8 bit) {
+  DQ = 1, DQ = 0, DQ = bit;
+  delay(40);
+}
 
-#define wire1_send_byte(byte, DQ)                                              \
-  do {                                                                         \
-    wire1_send_bit(byte & 0x01, DQ);                                           \
-    wire1_send_bit(byte & 0x02, DQ);                                           \
-    wire1_send_bit(byte & 0x04, DQ);                                           \
-    wire1_send_bit(byte & 0x08, DQ);                                           \
-    wire1_send_bit(byte & 0x10, DQ);                                           \
-    wire1_send_bit(byte & 0x20, DQ);                                           \
-    wire1_send_bit(byte & 0x40, DQ);                                           \
-    wire1_send_bit(byte & 0x80, DQ);                                           \
-  } while (0)
+static inline u8 wire1_recv_bit(void) {
+  DQ = 1, DQ = 0, DQ = 1;
+  delay(10);
+  u8 bit = DQ;
+  delay(30);
+  return bit;
+}
 
-#define wire1_recv_byte(out, DQ)                                               \
-  do {                                                                         \
-    wire1_recv_bit(out, 0, DQ);                                                \
-    wire1_recv_bit(out, 1, DQ);                                                \
-    wire1_recv_bit(out, 2, DQ);                                                \
-    wire1_recv_bit(out, 3, DQ);                                                \
-    wire1_recv_bit(out, 4, DQ);                                                \
-    wire1_recv_bit(out, 5, DQ);                                                \
-    wire1_recv_bit(out, 6, DQ);                                                \
-    wire1_recv_bit(out, 7, DQ);                                                \
-  } while (0)
+static inline void wire1_send_byte(u8 byte) {
+  wire1_send_bit(byte & 0x01);
+  wire1_send_bit(byte & 0x02);
+  wire1_send_bit(byte & 0x04);
+  wire1_send_bit(byte & 0x08);
+  wire1_send_bit(byte & 0x10);
+  wire1_send_bit(byte & 0x20);
+  wire1_send_bit(byte & 0x40);
+  wire1_send_bit(byte & 0x80);
+}
 
+static inline u8 wire1_recv_byte(void) {
+  bit8 byte;
+  byte.b0 = wire1_recv_bit();
+  byte.b1 = wire1_recv_bit();
+  byte.b2 = wire1_recv_bit();
+  byte.b3 = wire1_recv_bit();
+  byte.b4 = wire1_recv_bit();
+  byte.b5 = wire1_recv_bit();
+  byte.b6 = wire1_recv_bit();
+  byte.b7 = wire1_recv_bit();
+  return *(u8 *)&byte;
+}
 #endif
